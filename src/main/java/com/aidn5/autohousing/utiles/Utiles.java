9934eab74ea091e1d0f746623b63750a4f68205c
aidn3 <aidn.david3@gmail.com>
2018-06-22 22:53:59 +0200

    Add value sorter

--- src/main/java/com/aidn5/autohousing/utiles/Utiles.java
+++ src/main/java/com/aidn5/autohousing/utiles/Utiles.java
@@ -1,5 +1,13 @@
 package com.aidn5.autohousing.utiles;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import com.aidn5.autohousing.Config;
 
 public class Utiles {
@@ -37,5 +45,29 @@ public class Utiles {
 	 * 
 	 * return result; }
 	 */
+	static public Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean order) {
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(unsortMap.entrySet());
 
+		// Sorting the list based on values
+		Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+			@Override
+			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+
+				if (order) {
+					return o1.getValue().compareTo(o2.getValue());
+				} else {
+					return o2.getValue().compareTo(o1.getValue());
+
+				}
+			}
+		});
+
+		// Maintaining insertion order with the help of LinkedList
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for (Entry<String, Integer> entry : list) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+
+		return sortedMap;
+	}
 }
