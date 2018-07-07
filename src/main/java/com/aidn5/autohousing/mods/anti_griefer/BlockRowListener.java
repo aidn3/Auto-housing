20f18dd171d7eb26bf2c375eb7cc814d669b59b6
aidn3 <aidn.david3@gmail.com>
2018-07-07 14:25:30 +0200

    upgrading and finishing it

--- src/main/java/com/aidn5/autohousing/mods/anti_griefer/BlockRowListener.java
+++ src/main/java/com/aidn5/autohousing/mods/anti_griefer/BlockRowListener.java
@@ -5,59 +5,100 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
+import com.aidn5.autohousing.utiles.Message;
 import com.aidn5.autohousing.utiles.Utiles;
 
 public class BlockRowListener {
+	public int acceptedRange = 5;
+
 	private List<RowBlock> bRows;
+	private BlockingQueue<CBlock> queueblocks;
 
 	private Thread thread;
 	private Runnable runnable;
 
 	public BlockRowListener() {
+		bRows = new ArrayList<BlockRowListener.RowBlock>();
+		queueblocks = new ArrayBlockingQueue(5);
 
+		runnable = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+			}
+		};
+
+		thread = new Thread(runnable);
+		thread.start();
 	}
 
-	private void checkRows() {
+	public void Listener(int x1, int y1, int z1) {
 		try {
-			for (Iterator<RowBlock> iterator = bRows.iterator(); iterator.hasNext();) {
-				if ((iterator.next().last_block.time + 2000) < System.currentTimeMillis()) {
-					iterator.remove();
-				}
-			}
+			queueblocks.put(new CBlock(x1, y1, z1));
 		} catch (Exception e) {
 			Utiles.debug(e);
 		}
 	}
 
-	public void Listener(int x1, int y1, int z1) {
-		try {
-			boolean added = false;
+	private class queueblocksR implements Runnable {
+		private BlockingQueue<CBlock> queueblocks;
 
+		public queueblocksR(BlockingQueue<CBlock> queue) {
+			queueblocks = queue;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(30);
+
+					CBlock cBlock = queueblocks.take();
+					tryAdd(cBlock);
+					doRows();
+				} catch (Exception ignore) {}
+
+			}
+		}
+
+		private void tryAdd(CBlock cBlock) {
 			for (RowBlock rowBlock : bRows) {
 				if (rowBlock != null) {
-					if (rowBlock.addTry(x1, y1, z1)) added = true;
+					rowBlock.addTry(cBlock);
 				}
 			}
 
-			if (!added) {
-				RowBlock rBlock = new RowBlock(5);
-				rBlock.addTry(x1, y1, z1);
-				bRows.add(rBlock);
+			RowBlock rBlock = new RowBlock();
+			if (rBlock.addTry(cBlock)) bRows.add(rBlock);
+		}
+
+		private void doRows() {
+			try {
+				for (Iterator<RowBlock> iterator = bRows.iterator(); iterator.hasNext();) {
+					RowBlock cBlock = iterator.next();
+					if ((cBlock.last_block.time + 2000) < System.currentTimeMillis()) {
+						iterator.remove();
+					} else {
+						if (cBlock.Blocks.size() > 7) {
+							Message.showMessage("Anti-Griefer: " + cBlock.Blocks.size() + " blocks destroyed!");
+						}
+					}
+				}
+			} catch (Exception e) {
+				Utiles.debug(e);
 			}
-		} catch (Exception e) {
-			Utiles.debug(e);
 		}
 	}
 
 	private class RowBlock {
 		public List<CBlock> Blocks;
-		public int acceptedRange = 5;
 		public CBlock last_block;
 
-		public RowBlock(int acceptedRange_) {
+		public RowBlock() {
 			Blocks = new ArrayList();
-			acceptedRange = acceptedRange_;
 		}
 
 		public Map<String, Integer> getInRangePlayers() {
@@ -77,29 +118,22 @@ public class BlockRowListener {
 			return Utiles.sortByComparator(possiblePlayers, false);
 		}
 
-		public boolean addTry(double x, double y, double z) {
-			try {
-				if (Blocks.size() != 0) {
-					if (Utiles.Distance3D(last_block.X, last_block.Y, last_block.Z, x, y, z) > acceptedRange) {
-						return false;
-					}
+		public boolean addTry(CBlock cBlock) {
+
+			if (Blocks.size() != 0) {
+				if (Utiles.Distance3D(last_block.X, last_block.Y, last_block.Z, cBlock.X, cBlock.Y,
+						cBlock.Z) > acceptedRange) {
+					return false;
 				}
-				List<String> playersInRange = Main.playerListener.getPlayersInRange(acceptedRange, x, y, z);
-
-				CBlock cblock = new CBlock(x, y, z);
-				cblock.playersInRange = playersInRange;
-
-				last_block = cblock;
-				Blocks.add(cblock);
-			} catch (Exception e) {
-				Utiles.debug(e);
-				return false;
 			}
-			return false;
+
+			last_block = cBlock;
+			Blocks.add(cBlock);
+			return true;
 		}
 	}
 
-	class CBlock {
+	private class CBlock {
 		public double X;
 		public double Y;
 		public double Z;
@@ -113,6 +147,8 @@ public class BlockRowListener {
 			Z = z;
 
 			time = System.currentTimeMillis();
+
+			playersInRange = Main.playerListener.getPlayersInRange(acceptedRange, x, y, z);
 		}
 	}
 }
