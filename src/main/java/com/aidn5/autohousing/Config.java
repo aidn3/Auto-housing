3548d4e71766d1c94e2834eace7ad0da50768135
aidn3 <aidn.david3@gmail.com>
2018-07-21 13:05:13 +0200

    fix some minor bugs

--- src/main/java/com/aidn5/autohousing/Config.java
+++ src/main/java/com/aidn5/autohousing/Config.java
@@ -3,16 +3,16 @@ package com.aidn5.autohousing;
 public class Config {
 
 	// Mod settings
-	public static boolean debug_mode = true;
+	public static boolean debug_mode = false;
 
 	public static int cmd_timerS = 5000;
 	public static int cmd_timerF = 2000;
 	public static int refresh_Speed = 100;
 
 	public static int NEW_VERSION = 0;
+	public static String URL_UPDATER = "https://raw.githubusercontent.com/aidn3/Auto-housing/master/versions_json/update-0.5.json";
 	// public static String URL_UPDATER =
-	// "https://raw.githubusercontent.com/aidn3/Auto-housing/master/update.json";
-	public static String URL_UPDATER = "http://localhost/service/mod-auto-housing/update.json";
+	// "http://localhost/service/mod-auto-housing/update.json";
 
 	// Activated plug-ins
 	public static boolean HMod = true;
